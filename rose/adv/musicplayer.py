76fe6ceb83efdf7c95755879e8322ef2f710e827
TheProgramerkitty <99088658+TheProgramerkitty@users.noreply.github.com>
2022-02-07 22:33:23 +0530

    Update musicplayer.py

--- rose/adv/musicplayer.py
+++ rose/adv/musicplayer.py
@@ -35,7 +35,7 @@ Auth users can skip, pause, stop, resume Voice Chats even without Admin Rights.
 - /unauth `[Username or Reply to a Message]` : Remove a user from AUTH LIST of the group.
 - /authusers :  Check AUTH LIST of the group.
 
-[Old Manual Here ](https://t.me/szvcbot)
+
 """
 
 @app.on_callback_query(filters.regex("_adc"))
