3cf19ae59adebdf9d1ba4c833a55e8a97e6bea55
TheProgramerkitty <99088658+TheProgramerkitty@users.noreply.github.com>
2022-02-07 22:08:46 +0530

    Update videoplayer.py

--- rose/adv/videoplayer.py
+++ rose/adv/videoplayer.py
@@ -4,20 +4,20 @@ __HELP__ = """
 Rose can play video in your group's voice chat 
 zero lag time with high quality
 
-× /play - `[Youtube URL/Search Query/live URL or reply video/audion]` 
-× /pause - Pause the playing music on voice chat.
-× /resume - Resume the paused music on voice chat.
-× /skip- Skip the current playing music on voice chat
-× /end or /stop- Stop the playout.
+× /vplay - `[Youtube URL/Search Query/live URL or reply video/audion]` 
+× /vpause - Pause the playing music on voice chat.
+× /vresume - Resume the paused music on voice chat.
+× /vskip- Skip the current playing music on voice chat
+× /vend or /stop- Stop the playout.
 × /queue- Check queue list.
 × /playplaylist - Start playing Your Saved Playlist.
 × /playlist - Check Your Saved Playlist On Servers.
 × /delmyplaylist- Delete any saved music in your playlist
 × /delgroupplaylist- Delete any saved music in your group's playlist [Requires Admin Rights.]
 
-This is most advance music player in telegram base on open socure 
-music player repo rose can play music or video in your group video chat without lag...!
+This is most advance music player in telegram base on private Socure 
+music player repo Devil can play music or video in your group video chat without lag...!
 
-More info can found here - [click here](https://t.me/szvcbot/9)
+
  """
 __advtools__ = __HELP__
